8b2b8a973e8910d716a208511e205c8f9453aa3e
Ivan Perez <pumi0751@gmail.com>
2024-10-16 10:45:24 +0000

    Feat: finish task

--- src/js/app.js
+++ src/js/app.js
@@ -26,21 +26,45 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let cover = `<div class="cover"><img src=${
+    variables.includeCover
+      ? variables.background // Si includeCover es true, usa la imagen de fondo
+      : "https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/4G_LOGO_NEGRO-01__1_/original.png?1560209943" // Si includeCover es false, usa una imagen predeterminada
+  } /></div>`;
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  cover =
+    // reset the website body with the new html output
+    document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Lucy"} ${
+      variables.lastName ? variables.lastName : "Boillet"
+    }</h1>
+          <h2>${
+            variables.role !== null
+              ? variables.role
+              : "Filosofia de las profundidades"
+          }</h2>
+          <h3>${variables.city !== null ? variables.city : "Canarias"}, ${
+      variables.country !== null ? variables.country : "Spain"
+    }</h3>
+           <ul class="${
+             variables.socialMediaPosition === "position-left"
+               ? "position-left"
+               : "position-right"
+           }">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4GeeksAcademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4GeeksAcademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4geeksacademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
